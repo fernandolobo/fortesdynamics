2540957a950f681bcc2719bd55e181f7981aeb33
Fernando Lobo <fernando.lobo87@gmail.com>
2016-02-23 17:32:18 -0300

    Queue receiver

---
+++ Fortes.Dynamics/Mensageria/FortesAG/FortesAG.cs
@@ -0,0 +1,23 @@
+﻿using Mensageria.AGWebReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Mensageria.FortesAG
+{
+    public class AGService
+    {
+        private static IAGservice service  = new IAGservice();
+
+        public static IAGservice getIAGService()
+        {
+            if (service == null)
+            {
+                service = new IAGservice();
+            }
+            return service;
+        }
+    }
+}
--- Fortes.Dynamics/Mensageria/ScheduledTasks/QueueJob.cs
+++ Fortes.Dynamics/Mensageria/ScheduledTasks/QueueJob.cs
@@ -4,18 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Mensageria.AGWebReferenceLocal;
-using System.Text;
+using Mensageria.Queue;
+using Mensageria.FortesAG;
+using System.Threading.Tasks;
 
 namespace Mensageria.ScheduledTasks
 {
     public class QueueJob : IJob
     {
+
+
+
         public void Execute(IJobExecutionContext context)
         {
             try
             {
-                IAGservice AGService = new IAGservice();
-                var teste = AGService.Eco("Teste");
+                //string teste;
+                //IAGservice AGService = new IAGservice();
+                //Task.Run(() => teste = AGService.Eco(new MicrosoftQueue().MessageReceiver()));
+
+                string teste;
+                Task.Run(() => teste = AGService.getIAGService().Eco(new MicrosoftQueue().MessageReceiver()));
             }
             catch (Exception)
             {
